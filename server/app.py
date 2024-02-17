2d0d28948869de8f55087a21a019dc2ec4fd36a9
Cherono-mercy <cheronom142@gmail.com>
2024-02-17 23:34:41 +0800

    completed

--- server/app.py
+++ server/app.py
@@ -23,12 +23,40 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = []
+    for article in Article.query.all():
+        article_dict = article.to_dict()
+        articles.append(article_dict) 
+
+    response = make_response(
+        jsonify(articles),
+        200
+    ) 
+
+    return response   
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+#If this is the first request this user has made, set session["page_views"] to an initial value of zero using ternary operator
+    session["page_views"] = session.get("page_views") or 0
+    # For every request to '/articles/<int:id>' increment value of session["page_views"] by 1
+    session["page_views"] += 1
+    
+    # If the user has viewed 3 pages or fewer, render a JSON response with the article data
+    if session["page_views"] <= 3:
+        article = Article.query.filter_by(id = id).first()
+        article_dict = article.to_dict()
+
+        response = make_response(
+            jsonify(article_dict),
+            200
+        )
+        return response
+    
+    # If the user has viewed more than 3 pages, return a JSON response including an error message and status code of 401 unauthorized
+    return {"message": "Maximum pageview limit reached"}, 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
